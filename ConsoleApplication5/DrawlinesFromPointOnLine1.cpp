a6b3997b43df2413d3421f81a38456980f470314
jodi319 <jodi319@gmail.com>
2017-01-31 05:51:18 +0200

    DrawLinesFromPointOnLine1: completed minAreaRect = rotated bounding rectangle for components. completed-drawing perpendicular lines on baseline. WIP: determine length of perpendicular lines in order to draw them all at same lenth.

--- ConsoleApplication5/DrawlinesFromPointOnLine1.cpp
+++ ConsoleApplication5/DrawlinesFromPointOnLine1.cpp
@@ -15,6 +15,7 @@
 #include <array>
 #include "GraphUtils.h"
 #include "cvplot.h"
+#include "DrawlinesFromPointOnLine1.h"
 //#include <opencv2/legacy/compat.hpp>
 using namespace cv;
 using namespace std;
@@ -66,7 +67,7 @@ vector<Point> points1;
 vector<Point> points2;
 
 double angle_value_D = 45;
-int length = 100;
+int length = 10;
 Point firstLine_pt2;
 Point firstLine_pt1;
 Point pt4;
@@ -77,10 +78,14 @@ int lineSpacing = 20;
 //LineIterator it;
 //LineIterator it2;
 //vector<Vec3b> buf;
+int thresh = 4;
+int max_thresh = 255;
+RNG rng(12345);
 //![variables]
 
 void MyLine(Mat img, Point start, Point end)
 {
+	cout << "baseLine1= " << baseLine1 << ",  baseLine2= " << baseLine2 << "\n";
 
 	if (clickCounter ==/*1*/2)
 	{
@@ -125,7 +130,7 @@ void MyLine(Mat img, Point start, Point end)
 
 	lineCounter++;
 	drawCounter++;
-	cout << "centre= " << (Point(start.x, start.y) + Point(end.x, end.y)) / 2 << "\n";
+	//cout << "centre= " << (Point(start.x, start.y) + Point(end.x, end.y)) / 2 << "\n";
 
 	imshow("Drawn Image", img);
 }
@@ -144,12 +149,10 @@ void CallBackFunc(int event, int x, int y, int flags, void* userdata)
 		{
 			double theta = clickCounter *10;
 			int length = 150;
-			cout << "clickCounter= " << clickCounter << "\n";
 			pt1.x = x;
 			pt1.y = y;
 			baseLine1.x = pt1.x;
 			baseLine1.y = pt1.y;
-			cout << "Left button of the mouse is clicked - position (" << pt1.x << ", " << pt1.y << ")" << endl;
 
 			/////Draw line based pnone point (pt1, angle(theta) and length of line
 			//pt2.x = (int)round(x + length * cos(theta * CV_PI / 180.0));
@@ -160,37 +163,33 @@ void CallBackFunc(int event, int x, int y, int flags, void* userdata)
 		}
 		else if (clickCounter == 2)
 		{
-			/*drawCounter++;*/
 
-			////int tempX = pt2.x - pt1.x;
-			////int tempY = pt2.y - pt1.y;
-			////if (tempX <tempY)
-			////{
-			////	pt2.x = pt1.x;
-			////	pt2.y = y;
-			////}
-			////else if (tempX >tempY)
-			////{
-			////	pt2.x = x;
-			////	pt2.y = pt1.y;
-			////}
-			////else
-			////{
-			////	pt2.x = x;
-			////	pt2.y = y;
-			////}
-			//	pt2.x = x;
-			//	pt2.y = y;
-			////cout << "clickCounter= " << clickCounter << "\n";
-			////pt2.x = x;
-			////pt2.y = y;
-			//cout << "Left button of the mouse is clicked - position (" << pt2.x << ", " << pt2.y << ")" << endl;
+/*
+			int tempX = pt2.x - pt1.x;
+			int tempY = pt2.y - pt1.y;
+			if (tempX <tempY)
+			{
+				pt2.x = pt1.x;
+				pt2.y = y;
+			}
+			else if (tempX >tempY)
+			{
+				pt2.x = x;
+				pt2.y = pt1.y;
+			}
+			else
+			{
+				pt2.x = x;
+				pt2.y = y;
+			}
+				pt2.x = x;
+				pt2.y = y;
+			cout << "clickCounter= " << clickCounter << "\n";
+			pt2.x = x;
+			pt2.y = y;
+			cout << "Left button of the mouse is clicked - position (" << pt2.x << ", " << pt2.y << ")" << endl;*/
 
-#pragma region LineAnglePoint
-							//if (lineCounter==1 /* = 2 */)
-			//{
-			//	//pt1 = firstLine_pt1;
-			//	//pt2 = firstLine_pt2;
+#pragma region Perpendicular Line
 			//	////Direction vector going from 'start' to 'end'
 			//	//Point v;
 			//	//v.x = pt2.x - pt1.x;
@@ -213,100 +212,67 @@ void CallBackFunc(int event, int x, int y, int flags, void* userdata)
 			//	//pt4.x = pt2.x + v.x * (-length);
 			//	//pt4.y = pt2.y + v.y * (-length);  
 #pragma endregion
-
-
-			//	//pt1 = pt2;
-			//	//pt2 = pt3;
-			//}
 			pt2.x = x;
 			pt2.y = y;
 			baseLine2.x = pt2.x;
 			baseLine2.y = pt2.y;
 			MyLine(src, pt1, pt2);
 
-			////imshow("Drawn Image", src);
-			////lineCounter++;
+			//To pick up pixels under line
 			cv::LineIterator it(src_gray, pt1, pt2, 8);
-			//it2 = it;
+			//To store pixel picked up, from under line
 			std::vector<double> buf(it.count);
-			std::vector<double> objPixelbuf(it.count);
-			//std::vector<cv::Point> points(it.count);
-			////points;
-			////imshow("image", BinaryImg);
-			////cout << "line= " << lineCounter << " -- no. of pixels= " << it.count << "\n";
+
 			LineIterator it2 = it;
-			//cout << "it2.pos= " << it2.pos() << "\n";
-			//cout << "it.pos= " << it.pos() << "\n";
+
 			for (int i = 0; i < it.count; i++, ++it)
 			{
-				double val = (double)src_gray.at<uchar>(it.pos());
-				if (val == 255)
-				{
-					objPixelbuf[i] = val;
-					pixelCounter++;
-					points1.push_back(pt1);
-					points2.push_back(pt2);
-				}
-				
+				double val = (double)src_gray.at<uchar>(it.pos());				
 				buf[i] = val;
-				//cout << "pos= "<<it.pos()<<", value= "<<buf[i] << "\n";
 			}
-			//cout << "line= " << lineCounter << " -- no. of pixels in object= " << it.count << "\n";
-			allObjPixelsCount.push_back(pixelCounter);
-
-
-			////cout << "object pixel count= " << Mat(allObjPixelsCount) << "\n";
-
-
 			ofstream file;
 			file.open("buf.csv");
 			file << Mat(buf) << "\n";
 			file.close();
 
-			//if (lineCounter==2)
-			//{
-			//	////new line at 'end' pointing in opposite-v-direction
 
-			//	//pt1 = pt2;
-			//	//pt2 = pt4;
-			//	//MyLine(src, pt1, pt2);
-			//}
-#pragma region Plot Orthogonal Lines
-			int j = 1;
-			//cout << "** it.pos= " << it2.pos() << "\n";
+			int j = 0;
 			for (size_t i = 0; i < it.count; i++, ++it2)
-			{
+			{	// For the very 1st line at baseline's 1st coordinate
 				if (i == 0)
 				{
-					int j = i + 1;
+					cout << "1st LINE ON BASELINE:" << "\n";
+#pragma region Line Length=4
+					j++;
 					pt1 = it2.pos();
 					pt2 = Point(it2.pos().x + 100, it2.pos().y + 100);
-					cout << "pos= " << pt1 << "," << pt2 << "\n";
-					/*MyLine(src, pt1, pt2);*/
+					cout << "point from LineIterator= " << pt1 << "\n";
+#pragma endregion
 
+#pragma region Plot Orthogonal Lines
 					//Direction vector going from 'start' to 'end'
 					Point v;
-					v.x = baseLine2.x - pt1.x;
-					v.y = baseLine2.y - pt1.y;
+					v.x = baseLine2.x - baseLine1.x;
+					v.y = baseLine2.y - baseLine1.y;
 					//normalize v
-					int mag = sqrt(v.x*v.x + v.y*v.y);
+			/*		int mag = sqrt(v.x*v.x + v.y*v.y);
 					v.x = v.x / mag;
-					v.y = v.y / mag;
+					v.y = v.y / mag;*/
 					//rotate and swap
 					int tempX = -v.x;
 					v.x = v.y;
 					v.y = tempX;
 					//new line at 'end' pointing in v-direction
 					Point pt3;
-					pt3.x = pt1.x + v.x * length;
-					pt3.y = pt1.y + v.y * length;
-					//pt4.x = pt1.x + v.x * (-length);
-					//pt4.y = pt1.y + v.y * (-length);  
-					//re-align to pt1 & pt2
-					//pt1 = pt2;
+					pt3.x = pt1.x + v.x * 4;
+					pt3.y = pt1.y + v.y * 4;
+					////pt4.x = pt1.x + v.x * (-length);
+					////pt4.y = pt1.y + v.y * (-length);  
+					////re-align to pt1 & pt2
+					////pt1 = pt2;
 					pt2 = pt3;
-
-					cout << "pos= " << pt1 << "," << pt2 << "\n";
+#pragma endregion
+					//cout << "From pt1= " << pt1 << "  to  " << pt2 << "   (pt1 should be equal to baseline2)" << "\n";
 					MyLine(src, pt1, pt2);
 
 				}
@@ -314,7 +280,35 @@ void CallBackFunc(int event, int x, int y, int flags, void* userdata)
 				{
 					pt1 = it2.pos();
 					pt2 = Point(it2.pos().x + 100, it2.pos().y + 100);
-					cout << "pos= " << pt1 << "," << pt2 << "\n";
+					cout << "point from LineIterator= " << pt1 << "\n";
+
+#pragma region Plot Orthogonal Lines
+					////Direction vector going from 'start' to 'end'
+					Point v;
+					v.x = pt1.x - baseLine1.x;
+					v.y = pt1.y - baseLine1.y;
+					cout << "Direction vector going from 'start'-"<<pt1<<" to 'end'-"<<pt2 << "  = "<<v.x<<", "<<v.y<<"\n";
+					//normalize v
+					//int mag = sqrt(v.x*v.x + v.y*v.y);
+					//v.x = v.x / mag;
+					//v.y = v.y / mag;
+					//cout << "Normalizing v" << "\n";
+					//rotate and swap
+					int tempX = -v.x;
+					v.x = v.y;
+					v.y = tempX;
+					cout << "Rotating & swapping v" << "\n";
+					//new line at 'end' pointing in v-direction
+					pt4.x = pt1.x + v.x * (length);
+					pt4.y = pt1.y + v.y * (length);  
+					cout << "New line:" << "\n";
+					cout << "at" <<pt2<<" and "<< pt4<< "\n";
+					//re-align to pt1 & pt2
+					//pt1 = pt2;
+					pt2 = pt4;
+					cout << "Re-Aligning to pt1 & pt2" << "\n";
+					cout << "\n";cout << "\n";
+#pragma endregion
 					MyLine(src, pt1, pt2);
 					j++;
 				}
@@ -322,7 +316,6 @@ void CallBackFunc(int event, int x, int y, int flags, void* userdata)
 				{
 				}
 			}
-#pragma endregion
 
 		}
 		else
@@ -362,6 +355,46 @@ int main()
 	Mat tempSrcGray = src_gray;
 	imshow("grayscale image", src_gray);
 	setMouseCallback("grayscale image", CallBackFunc, NULL);
+	cv::Mat FltrBinaryImg = threshval < 128 ? (src_gray < threshval) : (src_gray > threshval);
+
+	Mat FltrLabelImage;
+	Mat FltrStats, FltrCentroids;
+	int nFltrLabels = cv::connectedComponentsWithStats(FltrBinaryImg, FltrLabelImage, FltrStats, FltrCentroids, 8, CV_32S);
+	string nFltrLabelsString = std::to_string(nFltrLabels);
+		std::vector<cv::Vec3b> FltrColors(nFltrLabels);
+		FltrColors[0] = cv::Vec3b(0, 0, 0);
+
+		for (int FltrLabel = 1; FltrLabel < 2/*nFltrLabels*/; ++FltrLabel)
+		{
+			FltrColors[FltrLabel] = cv::Vec3b((std::rand() & 255), (std::rand() & 255), (std::rand() & 255));
+			// Get the mask for the i-th contour
+			Mat mask_i = FltrLabelImage == FltrLabel;
+			string name = "mask_i_";
+			std::string s = std::to_string(FltrLabel);
+			//imwrite("mask_i_" + s + ".bmp", mask_i);
+			Mat Points;
+			findNonZero(mask_i, Points);
+			//Rect Min_Rect = boundingRect(Points);
+			//rectangle(mask_i, boundRect[i].tl(), boundRect[i].br(), color, 2, 8, 0);
+			/*rectangle(mask_i, Min_Rect.tl(), Min_Rect.br(), Scalar(0, 0, 255), 2);*/
+			// Find the minimum area enclosing bounding box
+			RotatedRect box = minAreaRect(Points);
+			Point2f vtx[4];
+
+			box.points(vtx);			
+			cout << "minAreaRect Angle= " << box.angle +180<< "\n";
+			for (size_t i = 0; i < 4; i++)
+			{
+				cout << "i= " << i << " -- " << vtx[i] << "\n";
+				cout << "vtx[(i + 1) % 4]= " << vtx[(i + 1) % 4] << "\n";
+				cout << "\n";
+			}
+			// Draw the bounding box
+			for (int i = 0; i < 4; i++)
+				line(src, vtx[i], vtx[(i + 1) % 4], Scalar(0, 255, 0), 1, LINE_AA);
+
+			imshow("Result", src);
+		}
 
 	waitKey(0);
 	return 0;
